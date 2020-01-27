830fcf703d1f750024f337cf5d52435edb46706b
giorgi01 <giorgi.tamarashvili.1@btu.edu.ge>
2020-01-27 21:27:18 +0400

    Added Admin & Sign in upon sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 class ApplicationController < ActionController::Base
-
-# Prevent CSRF attacks by raising an exception.
-# For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
   helper_method :current_user, :logged_in?
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
@@ -14,7 +11,7 @@ class ApplicationController < ActionController::Base
 
   def require_user
     if !logged_in?
-      flash[:notice] = "You must be logged in to perform that action"
+      flash[:danger] = "You must be logged in to perform that action"
       redirect_to root_path
     end
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
       flash[:notice] = "You have successfully logged in"
       redirect_to user_path(user)
     else
-      flash.now[:notice] = "There was something wrong with your login information"
+      flash.now[:danger] = "There was something wrong with your login information"
       render 'new'
     end
   end
